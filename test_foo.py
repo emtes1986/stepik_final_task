de2243ab2cdac56ab711d91d2e043ad520464468
emtes1986 <emtes@yandex.ru>
2021-01-26 18:56:27 +0300

    Rename test_foo.py to test_product_page.py

--- test_foo.py
+++ test_foo.py
@@ -1,27 +1,73 @@
 from .pages.product_page import ProductPage
 from .pages.base_page import BasePage
+from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
 from selenium import webdriver
 import pytest
 import time
 from selenium.common.exceptions import NoAlertPresentException
 from .pages.locators import ProductPageLocators
 
+class TestUserAddToBasketFromProductPage:
+    
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/" 
+        page = ProductPage(browser, link)
+        page.open()
+        time.sleep(1)
+        assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+           "Success message is presented, but should not be"
 
-#@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-  #                                "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-  #                                "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-   #                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-   #                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-   #                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-    #                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-    #                              pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-    #                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-    #                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/" 
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+   
+    @pytest.fixture(autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        email = str(time.time()) + "@fakemail.org"
+        password = "dvdkvjdk2324jk"
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+ 
 
-#def test_guest_can_add_product_to_basket(browser, link):
-   # page = ProductPage(browser, link)
-   # page.open()
-   # page.add_product_to_basket()
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+
+def test_guest_can_add_product_to_basket(browser, link):
+   page = ProductPage(browser, link)
+   page.open()
+   page.add_product_to_basket()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/" 
+    page = ProductPage(browser, link)
+    page.open()
+    time.sleep(1)
+    assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+        "Success message is presented, but should not be"
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.basket_should_be_empty()
+    basket_page.should_be_empty_message()
+
 
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -45,14 +91,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
        "Success message is presented, but should not be"
 
-def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/" 
-    page = ProductPage(browser, link)
-    page.open()
-    time.sleep(1)
-    assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
-       "Success message is presented, but should not be"
-
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket (browser): 
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/" 
@@ -62,3 +100,4 @@ def test_message_disappeared_after_adding_product_to_basket (browser):
     time.sleep(1)
     assert page.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message is presented, but not disappeared"
+
