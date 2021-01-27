6ff778fe3edfebff7dd57c9a651276525f3f914e
emtes1986 <76621367+emtes1986@users.noreply.github.com>
2021-01-27 23:21:33 +0300

    Update login_page.py
    Add method register_new_user(email, password)

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
+import time
 
 
 class LoginPage(BasePage):
@@ -7,6 +8,7 @@ class LoginPage(BasePage):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
+        self.register_new_user()
 
     def should_be_login_url(self):
         #assert self.browser.current_url == LoginPageLocators.LOGIN_URL, "Other login link"
@@ -18,15 +20,15 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_REGISTER_FORM), "Login registration form is not presented"
 
+    def register_new_user(self, email, password):
+        email_input = self.browser.find_element(*LoginPageLocators.EMAIL)
+        password1_input = self.browser.find_element(*LoginPageLocators.PASSWORD1)
+        password2_input = self.browser.find_element(*LoginPageLocators.PASSWORD2)
+        email_input.send_keys(email)
+        password1_input.send_keys(password)
+        password2_input.send_keys(password)
+        button_submit = self.browser.find_element(*LoginPageLocators.BUTTON_SUBMIT)
+        button_submit.click()
 
 
-    #def should_be_login_url(self):
-
-        #assert "login" in self.browser.current_url, "Other login link"
-
-    #def should_be_login_form(self):
-        #assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
-
-    #def should_be_register_form(self):
-       # assert self.is_element_present(*LoginPageLocators.LOGIN_REGISTER_FORM), "Login registration form is not presented"
 
