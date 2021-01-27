4b00e20ada164a63f56b455a52afd28047624f2f
emtes1986 <76621367+emtes1986@users.noreply.github.com>
2021-01-27 23:16:52 +0300

    Update product_page.py
    Page ready for review

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,42 +1,48 @@
-import math
 from .base_page import BasePage
 from .locators import ProductPageLocators
-from selenium.common.exceptions import NoAlertPresentException # в начале файла
+from selenium.common.exceptions import NoAlertPresentException
+import math
 
+class ProductPage(BasePage): 
 
-class ProductPage(BasePage):
-    def add_product_to_basket(self):
-        self.should_be_add_button()
-        self.click_add_button()
-        self.solve_quiz_and_get_code()
-        self.should_be_success_message()
-        self.should_be_correct_success_message()
-        self.should_be_price_message()
-        self.should_be_correct_price()
+    def get_product_name(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
 
-    def should_be_add_button(self):
-        assert self.is_element_present(*ProductPageLocators.ADDTOBASKET_BTN), "Add to cart button is not presented"  
+    def get_price(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
 
-    def click_add_button(self):
-        add_but = self.browser.find_element(*ProductPageLocators.ADDTOBASKET_BTN)
-        add_but.click()  
-    
-    def should_be_success_message(self):
-        assert self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "No success message presented"
+    def check_alert_product_added(self,product_name):
+        expected_alert=product_name+' has been added to your basket.'
+        alert = self.browser.find_elements(*ProductPageLocators.ALERT_MESSAGE)[0].text
+        assert expected_alert in alert, f"Should be '{expected_alert}' in alert:'{alert}'"
 
-    def should_be_correct_success_message(self):
-        title=self.browser.find_element(*ProductPageLocators.BOOK_TITLE)
-        a=title.text
-        success_message = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE)
-        b=success_message.text
-        assert a == b, f"Different title {title} and message {success_message}"
+    def check_alert_sum_in_basket(self,price):
+        expected_alert='Your basket total is now '+price
+        alert = self.browser.find_elements(*ProductPageLocators.ALERT_MESSAGE)[2].text
+        assert expected_alert in alert, f"Should be '{expected_alert}' in alert:'{alert}'"
 
-    def should_be_price_message(self):
-        assert self.is_element_present(*ProductPageLocators.PRICE_MESSAGE), "No price_message presented" 
+    def add_to_basket(self):
+        add_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_to_basket_button.click()
 
-    def should_be_correct_price(self):
-        price = self.browser.find_element(*ProductPageLocators.PRICE)
-        a = price.text
-        price_message = self.browser.find_element(*ProductPageLocators.PRICE_MESSAGE)
-        b=price_message.text
-        assert a == b, f"Different price {a} and message {b}"
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+           "Success message should not disappear"
+
+    def should_disappear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+           "Success message should disappear"
